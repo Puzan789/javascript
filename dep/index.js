8834ba8cc3c907d481a6e8e64496128c943d9c81
puzan789 <puzan936@gmail.com>
2023-01-23 00:00:20 +0545

    sfd

--- dep/index.js
+++ dep/index.js
@@ -5,7 +5,7 @@ const ipl = document.getElementById("ull");
 inputbtn.addEventListener("click", function () {
   cp = document.getElementById("input-el").value;
   mylead.push(cp);
-  cp = document.getElementById("input-el").value="";
+  cp = document.getElementById("input-el").value = "";
 
   renderleads();
 });
@@ -13,20 +13,18 @@ function renderleads() {
   let listitems = "";
 
   for (let i = 0; i < mylead.length; i++) {
-    if(mylead[i]=="")
-    {
-      null
-    }
-    else{
-    // listitems += "<li> <a href='"+mylead[i]+"' target='_blank'>" + mylead[i] +"</a> </li>";
-    listitems += `
+    if (mylead[i] == "") {
+      null;
+    } else {
+      // listitems += "<li> <a href='"+mylead[i]+"' target='_blank'>" + mylead[i] +"</a> </li>";
+      listitems += `
       <li class="list  mt-4" style="list-style:none;">
         <a class="text-success" target='_blank' href='${mylead[i]}'>
         ${mylead[i]}</a>
       </li>
 
     `;
+    }
   }
-}
   document.getElementById("ull").innerHTML = listitems;
 }
---
+++ new/templatestr.js
@@ -0,0 +1,7 @@
+let recipent='ritesh'
+const person ="genji"
+const show=`hey
+${recipent}
+ewqeqewrer cheers ${person}`
+
+console.log(show)
